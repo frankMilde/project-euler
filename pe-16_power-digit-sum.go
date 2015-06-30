e80d81f442b4704c1d1ebf7a5e810447165d9249
frankMilde <frankMilde@users.noreply.github.com>
2015-07-01 00:45:55 +0200

    Fix description

--- pe-16_power-digit-sum.go
+++ pe-16_power-digit-sum.go
@@ -2,7 +2,7 @@
 //    Description:  2^15 = 32768 and the sum of its digits is
 //                  3 + 2 + 7 + 6 + 8 = 26.
 //
-//       Question:  What is the sum of the digits of the number 21000?
+//       Question:  What is the sum of the digits of the number 2^1000?
 //
 //       Compiler:  go
 //
